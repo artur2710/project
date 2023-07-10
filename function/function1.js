5956d39c9735c0bc64786ebf9a661ef73a9079ef
gk <gk@adamant.local>
2023-07-10 20:55:35 +0300

    10.07

---
+++ function/function1.js
@@ -0,0 +1,58 @@
+/*let decimal = prompt('please insert decimal number');
+console.log(Number(decimal).toString(2));
+console.log(Number(decimal).toString());
+
+
+let decimal2 = prompt('please insert decimal number');
+console.log(Number(decimal2).toString(2));
+console.log(Number(decimal2).toString());
+
+
+let z = +decimal+ +decimal2;
+console.log(Number(z).toString(2));*/
+
+
+/*var myNunber=2;
+while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+}
+
+var x=2/0;
+var y=-2/0;*/
+
+
+/*var x = 123;
+var y = new Number(123);*/
+
+
+/*var x = 123;
+x.toString();
+(123).toString();
+(100 + 23).toString();*/
+
+
+/*var x = 9.656;
+x.toFixed(0);
+x.toFixed(2);
+x.toFixed(4);
+x.toFixed(6);
+console.log(x.toFixed(10));*/
+
+
+/*const x =10, y =-20;
+const z = Math.min(x,y);*/
+
+
+/*Math.pow(7,2);//49
+Math.pow(7,3);//343
+Math.pow(2,10);//1024
+
+console.log(Math.random());*/
+
+/*Math.floor(Math.random()*10);
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));*/
+
+console.log(Math.ceil(Math.random()*99));
+Math.floor(Math.random()*10);// до 9
+Math.floor(Math.random()*11);//до 10
