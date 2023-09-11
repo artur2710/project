ceb0266a568450efc4580278212dad7e86f5d4c0
gk <gk@adamant.local>
2023-09-11 20:59:32 +0300

    11.09

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,7 +1,7 @@
 const person = {
-    name:'Oleg',
-    age:25,
-    greet(){
+    name: 'Oleg',
+    age: 25,
+    greet() {
         console.log("Привет")
     }
 }
@@ -32,10 +32,54 @@ Object.prototype.sayHello = function() {
 }*/
 
 
-const anna = Object.create(person);
+/*const anna = Object.create(person);
 
 console.log(anna);
 console.log(anna.name);
 
 anna.name = "Anna";
-console.log(anna.name);
+console.log(anna.name);*/
+
+
+/*const parent = {
+    name: 'Stacey',
+    age:35,
+    heritage: 'Irish'
+}
+const child = Object.create(parent);
+console.log(child);
+console.log(child.name);
+child.name = 'Ryan';
+child.age = 7;
+console.log(child.name);//Ryan
+console.log(child.age);//7
+console.log(child.heritage);//Irish*/
+
+
+/*const animal = {
+    name: "Kot",
+    color: "grey",
+    class: "pats",
+    showInfo() {
+            console.log(this.name);
+            console.log(this.color);
+            console.log(this.class);
+        }
+    };
+
+const cat = Object.create(animal);
+cat.name = "Barsik";
+cat.color = "gray";
+const dog = Object.create(animal);
+dog.name = "Sharik";
+dog.color = "black";
+
+cat.showInfo();
+dog.showInfo();
+
+dog.show = function() {
+    console.log("hi");
+}
+dog.show();*/
+
+
